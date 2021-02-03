572dd07f0d92808bdc3c89d2874e955514aea3bd
Magnus Holta <magnus.holta@gmail.com>
2021-02-03 15:04:07 +0100

    #1: initialize repo with django

---
+++ backend/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
+
+
+# Create your views here.
+
+#class ProductView(generics.CreateAPIView):
+class ProductView(generics.ListAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
