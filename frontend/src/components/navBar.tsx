0f5e656965c434a684b3ff9e79602e04eabc783e
Frimann Garmann <Frimann98@gmail.com>
2021-02-17 09:48:55 +0100

    #17: fetch one/all users from userlist

--- frontend/src/components/navBar.tsx
+++ frontend/src/components/navBar.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+//import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from './SP-logo.png';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home" className="m-2">
+    <div /*bg="dark" variant="dark"*/>
+      <a href="#home" className="m-2">
         <img src={Logo} width="40" height="40" alt="" /> Sellpoint
-      </Navbar.Brand>
-    </Navbar>
+      </a>
+    </div>
   );
 };
 
