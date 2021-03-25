d123b37409d3e0c8576a8e0f13475f91db35c2c5
Richard Hornnæs Hovde <richarhh@stud.ntnu.no>
2021-03-25 14:02:48 +0100

    (#65)Set default value for product category

--- backend/category/models.py
+++ backend/category/models.py
@@ -2,5 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-  title = models.CharField( max_length=16, default="", unique=False)
-
+  title = models.CharField( max_length=16, default="", unique=True)
